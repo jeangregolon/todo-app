88d5c9bacb9e13337f25e64d37a02cd7c3d20e96
Jean Gregolon <jean.gregolon@gmail.com>
2021-05-31 22:45:11 -0300

    first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"/>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
